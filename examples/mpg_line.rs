4032970f730f26f0ecc870e0b8655a55739b3e05
Campbell He <kp.campbell.he@duskmoon314.com>
2025-01-03 16:52:55 +0800

    feat(example): aloow example run at workspace root

--- examples/mpg_line.rs
+++ examples/mpg_line.rs
@@ -1,15 +1,24 @@
+use std::{env, path::PathBuf};
+
 use gongbi::{aes, geom_line, plot};
 use polars::prelude::*;
 
 fn main() -> anyhow::Result<()> {
+    let examples_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("examples");
+    let mpg = examples_dir.join("mpg.csv");
+
     let mpg = CsvReadOptions::default()
         .with_has_header(true)
-        .try_into_reader_with_file_path(Some("examples/mpg.csv".into()))?
+        .try_into_reader_with_file_path(Some(mpg))?
         .finish()?;
 
-    let plot = plot!(mpg, aes!(hwy)) + geom_line!(aes!(label = "hwy"));
+    let plot = plot!(mpg.clone(), aes!(hwy)) + geom_line!(aes!(label = "hwy"));
 
-    plot.to_svg("mpg_line.svg", (1024, 768))?;
+    plot.to_svg(examples_dir.join("mpg_line.svg"), (1024, 768))?;
+
+    let plot = plot!(mpg.clone(), aes!(hwy)) + geom_line!(aes!(shape = 2));
+
+    plot.to_svg(examples_dir.join("mpg_line_shape.svg"), (1024, 768))?;
 
     Ok(())
 }
--- examples/mpg_point.rs
+++ examples/mpg_point.rs
@@ -1,23 +1,35 @@
+use std::{env, path::PathBuf};
+
 use gongbi::{aes, geom_point, labs, plot};
 use polars::prelude::*;
 
 fn main() -> anyhow::Result<()> {
+    let examples_dir = PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("examples");
+    let mpg = examples_dir.join("mpg.csv");
+
     let mpg = CsvReadOptions::default()
         .with_has_header(true)
-        .try_into_reader_with_file_path(Some("mpg.csv".into()))?
+        .try_into_reader_with_file_path(Some(mpg))?
         .finish()?;
 
     let plot = plot!(mpg.clone(), aes!("displ", "hwy"))
         + geom_point!()
         + labs!(caption = "Demo of geom_point");
 
-    plot.to_svg("mpg_point.svg", (1024, 768))?;
+    plot.to_svg(examples_dir.join("mpg_point.svg"), (1024, 768))?;
+    // plot.to_png(examples_dir.join("mpg_point.png"), (1024, 768))?;
 
     let plot = plot!(mpg.clone(), aes!("displ", "hwy", color = "blue"))
         + geom_point!()
         + labs!(caption = "Demo of geom_point with color");
 
-    plot.to_svg("mpg_point_color.svg", (1024, 768))?;
+    plot.to_svg(examples_dir.join("mpg_point_color.svg"), (1024, 768))?;
+
+    let plot = plot!(mpg.clone(), aes!("displ", "hwy"))
+        + geom_point!(aes!(shape = 0))
+        + labs!(caption = "Demo of geom_point with shape");
+
+    plot.to_svg(examples_dir.join("mpg_point_shape.svg"), (1024, 768))?;
 
     Ok(())
 }
